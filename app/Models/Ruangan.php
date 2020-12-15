977ecfa4454aacc1778e3352f458d3f5608d043c
TheAnarchyOne <73285456+TheAnarchyOne@users.noreply.github.com>
2020-12-15 21:58:32 +0700

    Update Ruangan.php

--- app/Models/Ruangan.php
+++ app/Models/Ruangan.php
@@ -11,10 +11,29 @@ class Ruangan extends Model
     
     protected $table = 'ruangan';
     protected $primaryKey = 'idruangan';
-    protected $fillable = ['nama','cs_id'];
+    protected $fillable = ['nama','cs_id', 'status'];
+
+
+    // public function user()
+    // {
+    //     return $this->belongsToMany(User::class);
+    // }
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // public function user()
+    // {
+    //     return $this->hasMany(User::class, 'cs_id', 'id');
+    // }
+
+    // eloquent orm --- 
     
     // SELECT Username
     // FROM users
     // INNER JOIN ruangan
     // ON users.id=ruangan.cs_id;
 }
+
