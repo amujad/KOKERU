ed40e20e416947065cae781f0a243b81b3d12158
amujad <49087569+amujad@users.noreply.github.com>
2020-12-16 07:13:48 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -174,6 +174,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        
+        //addition
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -212,6 +215,7 @@ return [
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
         'Password' => Illuminate\Support\Facades\Password::class,
         'Queue' => Illuminate\Support\Facades\Queue::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
