28a0cbab57d3df3b56a76241e39eaff9a1e45969
TheAnarchyOne <73285456+TheAnarchyOne@users.noreply.github.com>
2020-12-16 16:49:33 +0700

    Update dashboardManajer.blade.php

--- resources/views/dashboardManajer.blade.php
+++ resources/views/dashboardManajer.blade.php
@@ -156,6 +156,5 @@
         <script
             src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
             crossorigin="anonymous"></script>
-
     </body>
 </html>
