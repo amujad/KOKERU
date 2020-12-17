47f00ddd509c183344e86f5b39c2403c5431d1d6
syalwadeap <74665873+syalwadeap@users.noreply.github.com>
2020-12-17 11:51:07 +0700

    Update CsController.php

--- app/Http/Controllers/CsController.php
+++ app/Http/Controllers/CsController.php
@@ -10,6 +10,6 @@ class CsController extends Controller
     public function index()
     {
         $ruangan = DB::select('select r.nama, r.status, u.name from ruangan r, users u where r.cs_id=u.id');
-        return view('dashboardCs',['ruangan' => $ruangan]);
+        return view('dashboardCs',['ruangan' => $ruangan]); //controller cs
     }
 }
