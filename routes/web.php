36e2fe7a5c37ae6d2f55a8dd770f7786d7ed2af0
syalwadeap <74665873+syalwadeap@users.noreply.github.com>
2020-12-17 11:49:21 +0700

    update routenyaa

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('login','App\Http\Controllers\AuthController@index')->name('login');
 Route::post('proses_login','App\Http\Controllers\AuthController@proses_login')->name('proses_login');
 Route::get('logout','App\Http\Controllers\AuthController@logout')->name('logout');
 // auth
-// auth -> manajer || cs
+// auth -> manajer || cs level login
 
 Route::group(['middleware' => ['auth']], function () {
     Route::group(['middleware' => ['cek_login:manajer']], function () {
